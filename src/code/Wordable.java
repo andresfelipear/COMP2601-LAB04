2ea39aac93056ddb138713eb39b42fb5880bc642
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2024-06-08 15:00:11 -0700

    created main classes. finished a, b, c and d.

---
+++ src/code/Wordable.java
@@ -0,0 +1,17 @@
+/**
+ * Wordable
+ *
+ * @author Andres Arevalo & Raz
+ * @version 1.0
+ */
+public interface Wordable
+{
+    /**
+     * Creates a string based on the provided word and number.
+     *
+     * @param s the word to be used
+     * @param n the number to be used
+     * @return the string created based on the word and number
+     */
+    String createString(String s, int n);
+}
